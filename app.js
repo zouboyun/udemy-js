c88a906dccc1816abd3303cbdd46fd498eaf4a66
Boyun Zou <zouboyun@bu.edu>
2018-02-04 17:25:03 -0500

    math and operations

--- app.js
+++ app.js
@@ -94,6 +94,8 @@ console.log(typeof today);
 
 let val;
 
+// Anything to String you can either use String(yourdata) or (yourdata).toString()
+
 // Number to String
 
 val = String(5);
@@ -102,12 +104,63 @@ val = String(4+4);
 // Boolean to String
 
 val = String(true);
+val = (true).toString();
 
 // Object to String
-val = String(new Date());
-val = String([1, 2, 3]);
+// val = String(new Date());
+// val = String([1, 2, 3]);
 
+// Anything to Number you can either use Number(yourdata) or parseInt(yourdata) and parseFloat(yourdata)
 
+// String to Number
+
+val = Number('3.1415926'); // 3.1415926
+val = Number(false); // 0
+val = Number('hello'); // not a number
+val = Number([12,3]); // not a number
+val = parseInt('100.34'); // parse as a integer
+val = parseFloat('100.34'); // parse as a decimal
 console.log(val);
 console.log(typeof val);
 console.log(val.length);
+console.log(val.toFixed(6));
+
+// Type coercion
+
+const val1 = '5';
+const val2 = 6;
+
+const sum = Number(val1 + val2);
+
+console.log(sum);
+console.log(typeof sum);
+
+// Math and Operations
+
+const num1 = 101;
+const num2 = 50;
+let value;
+
+// math with numbers
+
+value = num1 + num2; // plus
+value = num1 - num2; // minus
+value = num1 * num2; // multiply
+value = num1 / num2; // divide
+value = num1 % num2; // residule
+value = Math.PI; // 3.14159
+value = Math.E; // natual exponential
+value = Math.round(2.4); // round to the nearest integer
+value = Math.ceil(2.4);
+value = Math.floor(2.4);
+value = Math.sqrt(3.69);
+value = Math.abs(-4);
+value = Math.pow(2, 3);
+value = Math.min(1, 2, 3);
+value = Math.max(1, 2, 3);
+value = Math.random(); // a random decimal
+
+console.log(value);
+
+// Strings and Concatenation
+
