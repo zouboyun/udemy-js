0937d30362e4ffea6c075323825bc3d2a1716534
Boyun Zou <zouboyun@bu.edu>
2018-03-18 16:17:59 -0400

    added tasks to local storage

--- Document_Object/DOM_Project/app.js
+++ Document_Object/DOM_Project/app.js
@@ -46,10 +46,28 @@ function addTask(e) {
   li.appendChild(link);
   // append the li to the ul
   taskList.appendChild(li);
+
+  // store in Local Storage
+  storeTaskInLocalStorage(taskInput.value);
+
   // clear the input
   taskInput.value = '';
-  e.preventDefault();}
+  e.preventDefault();
+  }
 }
+// Store Task
+function storeTaskInLocalStorage(task) {
+  let tasks;
+  if(localStorage.getItem('tasks') === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+
+  tasks.push(task);
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
 
 
 // Remove task
