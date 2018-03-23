0c3cb5bf4f835949b6e5dbef4e3f1b97af01ca77
Boyun Zou <zouboyun@bu.edu>
2018-03-22 23:43:38 -0400

    declare UI variables

--- Document_Object/Loan_Calculator/app.js
+++ Document_Object/Loan_Calculator/app.js
@@ -1,1 +1,7 @@
-alert('oops');
+//define UI vars
+
+const form = document.querySelector('#loan-form');
+const amount = document.querySelector('#amount');
+const interestRate = document.querySelector('#interest-rate');
+const yearToRepay = document.querySelector('#years-to-repay');
+const calculate = document.querySelector('#calculate');
