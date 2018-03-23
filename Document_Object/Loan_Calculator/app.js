7d8fe870eef05c42ae56c33f46227db744514ae0
Boyun Zou <zouboyun@bu.edu>
2018-03-22 23:48:30 -0400

    console log a few things to check

--- Document_Object/Loan_Calculator/app.js
+++ Document_Object/Loan_Calculator/app.js
@@ -5,3 +5,9 @@ const amount = document.querySelector('#amount');
 const interestRate = document.querySelector('#interest-rate');
 const yearToRepay = document.querySelector('#years-to-repay');
 const calculate = document.querySelector('#calculate');
+
+console.log(form);
+console.log(amount);
+console.log(interestRate);
+console.log(yearToRepay);
+console.log(calculate);
