49d3b3284555fd0a8badeadc5fe9834f42d462a1
Boyun Zou <zouboyun@bu.edu>
2018-03-25 23:14:38 -0400

    added calculation part

--- Document_Object/Loan_Calculator/app.js
+++ Document_Object/Loan_Calculator/app.js
@@ -17,10 +17,33 @@ function loadEventListerners() {
 function calculateRate(e) {
   // UI variables for calculation
   const amount = document.querySelector('#amount');
+  const downPayment = document.querySelector('#down-payment');
   const interestRate = document.querySelector('#interest-rate');
   const yearToRepay = document.querySelector('#years-to-repay');
   const monthlyPayment = document.querySelector('#monthly-payment');
   const totalPayment = document.querySelector('#total-payment');
   const totalInterest = document.querySelector('#total-interest');
+
+  // create value vars
+  const dp = parseFloat(downPayment.value); // parseFloat makes it a float type otherwise it will be string
+  const principal = parseFloat(amount.value - dp);
+  const calInterest = parseFloat(interestRate.value) / 100 / 12;
+  const calFrequency = parseFloat(yearToRepay.value) * 12;
+  
+  // monthly payment
+  const x = Math.pow(1 + calInterest, calFrequency);
+  const p = calInterest * x * principal / (x - 1);
+
+
+  if (isFinite(p)) {
+    monthlyPayment.value = p.toFixed(2); // set decimal place to be 2
+    totalPayment.value = ((p * calFrequency) + dp).toFixed(2);
+    totalInterest.value = ((p * calFrequency) - principal).toFixed(2);
+  } else {
+    alert('oops wrong number');
+  }
+
+
+
   e.preventDefault();
 }
