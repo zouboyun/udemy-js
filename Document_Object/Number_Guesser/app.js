7c8b2266b19ab59c9d8ef44ff3602fcc7ab07e29
Boyun Zou <zouboyun@bu.edu>
2018-05-17 12:42:14 -0700

    new skeleton project

---
+++ Document_Object/Number_Guesser/app.js
@@ -0,0 +1,8 @@
+/*
+- Number Guesser will allow users to guess from a certain range of numbers
+- Users will have 3 chances to guess the correct result
+- Users will get a message in red if the answer is incorrect
+- Users will get a message in green if the answer is correct
+- Will be using skeleton in this project
+*/
+
