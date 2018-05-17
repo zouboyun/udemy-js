8bf31034416d0ad91331ef84350c3fad86a78950
Boyun Zou <zouboyun@bu.edu>
2018-05-17 12:21:48 -0700

    practice

--- JS_Practice/app.js
+++ JS_Practice/app.js
@@ -28,6 +28,48 @@ function main() {
   alert(reduced(product)(1, doubledFib));
   alert(reduced(product)(1, mapped(triple)(fib))); // mapped triple fib -> 3, 6, 9, 15, 24
 }
-//main();
+// Test Setup
+// var Mocha = require('mocha')
+// var assert = require('assert')
+// var mocha = new Mocha()
 
+// mocha.suite.emit('pre-require', this, 'solution', mocha)
+
+/* your code here*/
+
+// given a list of non-negative integers and a target sum, find a pair of numbers that sums to the target sum
+
+function pairsSum(numList, sum) {
+  if (! (numList instanceof Array)) {
+    throw new Error('Please input an array');
+  }
+  if (! (numList.every(x => typeof x == 'number'))) {
+    throw new Error('Please input integers in your array');
+  }
+  if (Math.min(...numList) > sum) {
+    return [];
+  }
+  if (Math.min(...numList) == sum) {
+    return [Math.min(...numList)];
+  }
+  for (var i = 0; i < numList.length; i++) {
+    for (var j = i + 1; j < numList.length; j++) {
+      if (numList[i] + numList[j] == sum) {
+        return [numList[i], numList[j]];
+      }
+    }
+  }
+}
+
+console.log(pairsSum([1, 2, 3], 5)) // should return [1, 3]
+// describe('Find Pairs of Sum Function', function() {
+//   it('Should return true when input a list of non-negative integers', function() {
+//     var testList = [1, 2, 3];
+//     assert(pairsSum(testList));
+//   })
+// })
+
+/* end of your code*/
+
+// mocha.run();
 
