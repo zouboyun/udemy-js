d5fde21e8c31e64073d134373a1b5195d87dca08
Boyun Zou <zouboyun@bu.edu>
2018-05-17 16:20:39 -0700

    refactored code to disable input, set border color and set message

--- Document_Object/Number_Guesser/app.js
+++ Document_Object/Number_Guesser/app.js
@@ -35,17 +35,33 @@ guessBtn.addEventListener('click', function() {
   if (isNaN(guess) || guess < min || guess > max) {
     setMsg(`Please input a number betwen ${min} and ${max}`, 'red');
   }
-  // add message for winning
+  // add message for winning and losing
   if (guess === winNum) {
-    // disable input
-    guessInput.disabled = true;
-    // change border color
-    guessInput.style.bordercolor = 'green';
-    // set message
-    setMsg(`That's a bingo! ${winNum} is correct.`, 'green');
+    gameOver(true, `That's a bingo! ${winNum} is correct.`);
+  } else {
+      guessLeft -= 1;
+      if (guessLeft === 0) {
+      // game over if guess left is 0
+      gameOver(false, `Game over! You lost. ${winNum} is the answer.`);
+     } else {
+        // game continues
+        // set border color to red
+        guessInput.style.borderColor = 'red';
+        // set message to notify guesses left
+        setMsg(`Guess is not correct. ${guessLeft} guesses left`, 'red');
+        // clear input
+        guessInput.value = '';
+      }
   }
 });
 
+function gameOver(won, msg) {
+  let color;
+  won === true ? color = 'green' : color = 'red';
+  guessInput.disabled = true;
+  guessInput.style.borderColor = color;
+  setMsg(msg, color);
+}
 function setMsg(message, color) {
   msg.textContent = message;
   msg.style.color = color;
