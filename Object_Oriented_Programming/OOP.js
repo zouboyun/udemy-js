670a51b67e202987ca75ee75c74b29bbb75a3995
Boyun Zou <zouboyun@bu.edu>
2018-05-18 14:13:22 -0700

    created a Person object and manipulated properties

--- Object_Oriented_Programming/OOP.js
+++ Object_Oriented_Programming/OOP.js
@@ -1,9 +1,14 @@
 // Person constructor
 
-function Person() {
-  this.name = 'Alice';
-}
+function Person(name, dob) {
+  this.name = name;
+  this.birthday = new Date(dob);
+  this.age = new Date().getFullYear() - this.birthday.getFullYear();
+};
 
-const alice = new Person();
 
-console.log(alice);
+const alice = new Person('Alice', '11/29/2001');
+const john = new Person('John', '9/19/1987');
+
+console.log(alice.age);
+console.log(john.age);
