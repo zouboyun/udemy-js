bb7ea38ecf8221c905f446b1b7336ad39ec9f242
Boyun Zou <zouboyun@bu.edu>
2018-05-21 12:23:14 -0700

    manipulated XmlHttpRequest object

---
+++ Async_JavaScript/Async.js
@@ -0,0 +1,18 @@
+document.getElementById('get-data').addEventListener('click', loadData);
+
+function loadData() {
+  // instantiate an XmlHttpRequest object
+  const xhr = new XMLHttpRequest();
+  // initializes a newly-created request, method GET, async is true, can also pass in user and password but we don't need those for now
+  xhr.open('GET', 'data.txt', true);
+  // this is a callback to be executed when the request is completed successfully
+  xhr.onload = function() {
+    // check status code to make sure it is 200 (OK)
+    if (this.status === 200) {
+      console.log(this.responseText);
+    }
+  }
+  //The XMLHttpRequest method send() sends the request to the server
+  xhr.send();
+
+}
