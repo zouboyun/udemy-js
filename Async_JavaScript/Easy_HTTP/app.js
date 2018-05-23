97c11213620199c9f9369c90f6d3659803fbb080
Boyun Zou <zouboyun@bu.edu>
2018-05-22 21:46:51 -0700

    added post method

--- Async_JavaScript/Easy_HTTP/app.js
+++ Async_JavaScript/Easy_HTTP/app.js
@@ -2,4 +2,23 @@
 const http = new EasyHTTP();
 
 // get posts
-http.get('https://jsonplaceholder.typicode.com/posts');
+// http.get('https://jsonplaceholder.typicode.com/posts', function(err, response) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(JSON.parse(response));
+//   }
+// });
+
+// post new post
+const data = {
+  title: 'Custom Popo',
+  body: 'This is a test post.'
+}
+http.post('https://jsonplaceholder.typicode.com/posts', data, function(err, post) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(JSON.parse(post));
+  }
+});
--- Async_JavaScript/Easy_HTTP/easyHTTP.js
+++ Async_JavaScript/Easy_HTTP/easyHTTP.js
@@ -4,18 +4,34 @@ function EasyHTTP() {
 }
 
 // Make an HTTP GET Request
-EasyHTTP.prototype.get = function(url) {
+EasyHTTP.prototype.get = function(url, callBack) {
   this.xhr.open('GET', url, true);
   // this cannot be passed into the onload call back
   let self = this;
   this.xhr.onload = function() {
     if (self.xhr.status === 200) {
-      return self.xhr.responseText;
+      callBack(null, self.xhr.responseText);
+    } else {
+      callBack('Error: ' + self.xhr.status);
     }
   }
   this.xhr.send();
 }
+
 // Make an HTTP POST Request
+EasyHTTP.prototype.post = function(url, data, callBack) {
+  this.xhr = new XMLHttpRequest();
+  this.xhr.open('POST', url, true);
+  let self = this;
+  this.xhr.onload = function() {
+    if (self.xhr.status === 201) {
+      callBack(null, self.xhr.responseText);
+    } else {
+      callBack('Error: ' + self.xhr.status);
+    }
+  }
+  this.xhr.send(JSON.stringify(data));
+}
 
 // Make an HTTP PUT Request
 
