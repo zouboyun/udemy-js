69d68ef88a75f392a44d0f3cab6ffe9fdf426e53
Boyun Zou <zouboyun@bu.edu>
2018-05-22 19:49:56 -0700

    using callback to async

--- Async_JavaScript/CallBack.js
+++ Async_JavaScript/CallBack.js
@@ -1,13 +1,41 @@
 // mocking server behavior by setting 2s to create post and 1s to get post
 
+// const posts = [
+//   {title: 'Post One', body: 'This is post one.'},
+//   {title: 'Post Two', body: 'This is post two.'}
+// ];
+
+// function createPost(post) {
+//   setTimeout(function() {
+//     posts.push(post);
+//   }, 2000);
+// }
+
+// function getPost() {
+//   setTimeout(function() {
+//     let output = '';
+//     posts.forEach(function(post) {
+//       output += `<li>${post.title}</li>`;
+//     })
+//     document.body.innerHTML = output;
+//   }, 1000);
+// }
+
+// createPost({title: 'Post Three', body: 'This is post three.'});
+// getPost();
+
+
+// using callback to async
+
 const posts = [
   {title: 'Post One', body: 'This is post one.'},
   {title: 'Post Two', body: 'This is post two.'}
 ];
 
-function createPost(post) {
+function createPost(post, callBack) {
   setTimeout(function() {
     posts.push(post);
+    callBack();
   }, 2000);
 }
 
@@ -21,13 +49,4 @@ function getPost() {
   }, 1000);
 }
 
-function updatePost() {
-
-}
-
-function deletePost() {
-
-}
-
-createPost({title: 'Post Three', body: 'This is post three.'});
-getPost();
+createPost({title: 'Post Three', body: 'This is post three.'}, getPost);
