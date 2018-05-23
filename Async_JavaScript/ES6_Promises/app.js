4062827bfa44e2aaac81fc377515d4316c164e20
Boyun Zou <zouboyun@bu.edu>
2018-05-23 15:05:46 -0700

    ES6 promises

---
+++ Async_JavaScript/ES6_Promises/app.js
@@ -0,0 +1,39 @@
+const posts = [
+  {title: 'Post One', body: 'This is a post.'},
+  {title: 'Post Two', body: 'This is a post.'}
+]
+
+// use promise in createPost
+// it is called promise because when certain things are done, some other things will happen like promised
+// when resolve, we call some function; when reject, we show error
+function createPost(post) {
+  return new Promise(function(resolve, reject) {
+    setTimeout(function() {
+      posts.push(post);
+      const error = false;
+      if (!error) {
+        resolve('You got it.');
+      } else {
+        reject('Something went wrong.')
+      }
+    }, 2000)
+  })
+}
+
+function getPosts(msg) {
+  setTimeout(function() {
+    let output = '';
+    posts.forEach(function(post) {
+      output += `<li>${post.title}</li>`
+    });
+    document.body.innerHTML = output;
+  }, 1000);
+  console.log(msg);
+}
+
+// on resolve call the callback in then; on reject call the call back in catch
+createPost({title: 'Post Three', body: 'This is a post.'})
+.then(getPosts)
+.catch(function(err) {
+  console.log(err);
+});
