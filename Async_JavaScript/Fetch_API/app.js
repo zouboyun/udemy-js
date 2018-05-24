1c4f1d885c4f94daf29560837b7d380129d68335
Boyun Zou <zouboyun@bu.edu>
2018-05-23 17:10:48 -0700

    refactored code with arrow functions

--- Async_JavaScript/Fetch_API/app.js
+++ Async_JavaScript/Fetch_API/app.js
@@ -8,49 +8,52 @@ document.getElementById('button3').addEventListener('click', getApiData);
 function getTxtData() {
   // fetch function returns a promise
   fetch('test.txt')
-    .then(function(response) {
-      return response.text();  
-    })
-      .then(function(data) {
-        document.getElementById('output1').innerHTML = data;
-      })
-    .catch(function(err) {
-      console.log(err);
-    })
+    .then(response => response.text())
+      .then(data => document.getElementById('output1').innerHTML = data)
+    .catch(err => console.log(err))
 }
 
 function getJsonData() {
   // fetch function returns a promise
   fetch('test.json')
-    .then(function(response) {
-      return response.json();  
-    })
-      .then(function(data) {
+    .then(response => response.json())
+      .then(data => {
         let output = '';
-        data.forEach(function(obj) {
-          output += `<li>${obj.title}</li>`
-        })
+        data.forEach(obj => output += `<li>${obj.title}</li>`)
         document.getElementById('output2').innerHTML = output;
       })
-    .catch(function(err) {
-      console.log(err);
-    })
+    .catch(err => console.log(err))
 }
 
 function getApiData() {
   // fetch function returns a promise
   fetch('https://api.github.com/users')
-    .then(function(response) {
-      return response.json();  
-    })
-      .then(function(data) {
+    .then(response => response.json())
+      .then(data => {
         let output = '';
-        data.forEach(function(user) {
-          output += `<li>${user.login}</li>`
-        })
+        data.forEach(user => output += `<li>${user.login}</li>`)
         document.getElementById('output3').innerHTML = output;
       })
-    .catch(function(err) {
-      console.log(err);
-    })
+    .catch(err => console.log(err))
 }
+
+// arrow function does not need key word function
+const testArrow = () => {console.log('success')};
+// arrow function -- one line function does not need {}
+const sayHello = (name, age) => console.log(`Hello, ${name}'s age is ${age}`);
+// arrow function -- one parameter does not need ()
+const testOnePar = name => console.log(`Yo ${name}`);
+// arrow fucntion -- do not need the return key word, for object need ()
+const sayHalo = () => ({msg: 'Halo'});
+// arrow function -- can be used as call back
+const users = ['Alice', 'John', 'Eric'];
+const nameLength = users.map(name => name.length);
+
+
+
+// testArrow()
+// sayHello('tj', 30);
+// testOnePar('tj');
+// console.log(sayHalo());
+// console.log(nameLength);
+// console.log(users)
