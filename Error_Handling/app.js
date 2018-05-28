9194e207131dbe41177ffe7293c9c97a217f9fdc
Boyun Zou <zouboyun@bu.edu>
2018-05-28 16:19:25 -0700

    went through reference type and syntax error

--- Error_Handling/app.js
+++ Error_Handling/app.js
@@ -1,7 +1,11 @@
 // using try and catch, the error will be caught meanwhile the program won't stop running
 try {
   // this will be a reference error, myFunction is not declared / defined
-  myFunction();
+  // myFunction();
+  // this will be a type error
+  // null.myFunction();
+  // evaluate js code
+  eval('"hello');
 } catch(e) {
   // see which type of error it is
   console.log(e.name);
@@ -10,6 +14,8 @@ try {
   // returns boolean
   console.log(e instanceof TypeError);
   console.log(e instanceof ReferenceError);
+} finally {
+  console.log('finally...')
 }
 
 console.log('Program continues...');
