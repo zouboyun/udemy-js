6e6ee6c507aaac9c7a513469aba7b356451bbdf2
Boyun Zou <zouboyun@bu.edu>
2018-05-29 15:47:17 -0700

    character sets

--- Regex_Notes.js
+++ Regex_Notes.js
@@ -2,30 +2,48 @@
 let re = /^Hello$/; // hello would NOT be a match in this case
 
 /* i means case insensitive */
-let re = /^Hello$/i; // hello would be a match in this case
+re = /^Hello$/i; // hello would be a match in this case
 
 /* g means global search, meaning the search will apply to the whole paragraph */
-let re = /^Hello$/g;
+re = /^Hello$/g;
 
 /* ^ -- must start with */
-let re = /^Hello/; // the match must start with 'Hello'
+re = /^Hello/; // the match must start with 'Hello'
 
 /* $ -- must end with  */
-let re = /Hello$/; //the match must end with 'Hello'
+re = /Hello$/; //the match must end with 'Hello'
 
 /* . -- you can put any ONE character */
-let re = /H.llo/; // 'Hollo' or 'H)llo' will be a match in this case; 'Hoollo' will NOT be a match
+re = /H.llo/; // 'Hollo' or 'H)llo' will be a match in this case; 'Hoollo' will NOT be a match
 
 /* * -- you can put any 0 or more characters */
-let re = /H*llo/; // 'Hllo' and 'Hoollo' will be a match in this case
+re = /H*llo/; // 'Hllo' and 'Hoollo' will be a match in this case
 
 /* ? -- optional character, will match with or without but cannot be replaced by other character */
-let re = /He?a?llo/; // e and a are optional in this case, 'Hello', 'Hallo', 'Hllo' will be a match; 'Hullo' will NOT be a match
+re = /He?a?llo/; // e and a are optional in this case, 'Hello', 'Hallo', 'Hllo' will be a match; 'Hullo' will NOT be a match
 
 /* \ -- escape character */
-let re = /grey?/; // here y is optional
-let re = /grey\?/; // here y is NOT optional and ? is also part of the search
+re = /grey?/; // here y is optional
+re = /grey\?/; // here y is NOT optional and ? is also part of the search
 
-/*  */
+/* [] -- character sets */
+re = /gr[ae]y/i; // it has to be an a or an e, cannot be empty; 'gray' or 'grey' will be a match, 'gry' will NOT
 
-/*  */
+/* [^] -- anything except this */
+re = /[^GF]rey/; // anything starts with G or F will NOT be a match
+/* [-] -- range of letters or numbers */
+re = /[A-Z]/; // match any upper case letter
+re = /[a-z]/; // match any lower case letter
+re = /[A-Za-z]/; // match any letter
+re = /[0-9]/; // match any digit
+
+/* {} -- quantifiers */
+re = /Hel{2}o/i; // this is looking for 2 'l's to match, the number inside of {} specifies how many of the character in front should be; must appear exactly {n} times
+
+re = /Hel{2,4}o/i; // 2 - 4 is the range of the amount that 'l' has to appear
+
+re = /Hel{2,}o/i; // must appear at least two times
+
+/* () --  grouping */
+re = /([0-9]x){3}/; // look for (number + x) to appear three times
+
